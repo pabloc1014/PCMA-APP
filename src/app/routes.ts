c3a550f656e92ec555871602643c7454cedf3851
Pablo <pablo.cogua10@gmail.com>
2019-10-05 15:44:21 -0500

    Welcome page updated

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DatesComponent } from './dates/dates.component';
+import { Component } from '@angular/core';
+import { LoginComponent } from './user/login.component';
 
 export const appRoutes:Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'dates', component: DatesComponent},
+    { path: 'login', component:LoginComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full'},
 ]
